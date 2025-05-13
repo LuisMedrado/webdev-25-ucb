855109d09b82c815488efb2418d845f03730d7fd
LuisMedrado <luplfmc@gmail.com>
2025-05-13 10:49:53 -0300

    fix html name att

--- atividade5/index.js
+++ atividade5/index.js
@@ -5,12 +5,18 @@ const app = express();
 app.engine('html', mustacheExpress())
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views')
+app.use(express.urlencoded({extended: true}))
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
     console.log("Rodou na porta " + PORT)
 })
 
-app.get('/agendamento', (req, res)=> {
+app.get('/', (req, res)=> {
+    res.render('index.html')
+})
+
+app.post('/agendar_consulta', (req, res)=> {
+    console.log(req.body)
     res.render('index.html')
 })
